685f360dc25f4f03b7309217f32556c694318340
LYSPbYeasf <99254444+LYSPbYeasf@users.noreply.github.com>
2022-03-14 16:02:21 +0300

    Add files via upload

---
+++ LAB ASP_MVC/Lab01_упр2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //return View();
+        //}
+        public string Index(string hel)
+        { 
+            int hour = DateTime.Now.Hour;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день" + ", " +hel; 
+            return Greeting; 
+        }
+    }
+}
