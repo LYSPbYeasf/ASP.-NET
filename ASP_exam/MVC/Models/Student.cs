9a1e5dcb98757d0bb82377842eeb20ad98d73164
Liudmila Yaperova <99254444+LYSPbYeasf@users.noreply.github.com>
2022-03-20 20:46:14 +0300

    Add files via upload

---
+++ ASP_exam/MVC/Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebMVC.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "Заполните поле имени")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Заполните поле фамилия")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Заполните поле группа")]
+        public string group { get; set; }
+        
+        public int winFormAssessment { get; set; }
+        
+        public int cAssessment { get; set; }
+        
+        public int aspAssessment { get; set; }
+        
+        public int sqlAssessment { get; set; }
+        
+        public int adoAssessment { get; set; }
+        
+        public int sumAssessment { get; set; }
+    }
+}
---
+++ ASP_exam/MVC/Models/StudentDbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebMVC.Models
+{
+    public class StudentDbInitializer : DropCreateDatabaseAlways<StudentContext>
+    {
+        protected override void Seed(StudentContext db)
+        {
+            db.Students.Add(new Student { firstName = "Александр", lastName = "Смирнов", group = "1", winFormAssessment = 3, cAssessment = 4, aspAssessment = 3, sqlAssessment = 4, adoAssessment = 5 });
+            db.Students.Add(new Student { firstName = "Дмитрий", lastName = "Кузьмин", group = "2", winFormAssessment = 4, cAssessment = 5, aspAssessment = 4, sqlAssessment = 4, adoAssessment = 5 });
+            db.Students.Add(new Student { firstName = "Ева", lastName = "Кудрина", group = "3", winFormAssessment = 5, cAssessment = 4, aspAssessment = 4, sqlAssessment = 4, adoAssessment = 5 });
+            db.Students.Add(new Student { firstName = "Ирина", lastName = "Василеа", group = "1", winFormAssessment = 4, cAssessment = 4, aspAssessment = 5, sqlAssessment = 3, adoAssessment = 3 });
+            db.Students.Add(new Student { firstName = "Марина", lastName = "Смирнова", group = "2", winFormAssessment = 5, cAssessment = 3, aspAssessment = 4, sqlAssessment = 4, adoAssessment = 4 });
+            db.Students.Add(new Student { firstName = "Богдан", lastName = "Городин", group = "2", winFormAssessment = 3, cAssessment = 4, aspAssessment = 3, sqlAssessment = 3, adoAssessment = 4 });
+
+            base.Seed(db);
+        }
+    }
+}
